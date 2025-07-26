5e545d0f91131e8172aa6f1fbf60e02cbadb42c5
ChubbyChuckles <christian.rickert.1989@gmail.com>
2025-07-26 22:52:10 +0200

    Automated commit: 2025-07-26 22:52:10

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,33 +1,41 @@
 import os
 import sys
 
-# Add project root to sys.path (adjust path to point to Categorical Trading)
-sys.path.insert(0, os.path.abspath("../.."))
+sys.path.insert(
+    0, os.path.abspath("../..")
+)  # Adjust to point to your project root
 
-# Project information
-project = "Categorical Trading"
-copyright = "2025, Chuck"
-author = "Chuck"
-release = "0.1"
-
-# General configuration
 extensions = [
-    "sphinx.ext.autodoc",  # For auto-generating documentation from docstrings
-    "sphinx.ext.napoleon",  # For Google-style docstrings
+    "sphinx.ext.autodoc",  # For documenting Python code
+    "sphinx.ext.napoleon",  # For Google/NumPy-style docstrings
     "sphinx.ext.viewcode",  # For linking to source code
 ]
-templates_path = ["_templates"]
-exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
-# HTML output options
+html_theme = "sphinx_rtd_theme"
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = "project-template"
+copyright = "2025, Christian Rickert"
+author = "Christian Rickert"
+release = "1.0.0"
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ["_templates"]
+exclude_patterns = []
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
 html_theme = "alabaster"
 html_static_path = ["_static"]
-
-# Autodoc settings
-autodoc_default_options = {
-    "members": True,
-    "undoc-members": True,
-    "show-inheritance": True,
-}
-autodoc_typehints = "description"  # Include type hints in descriptions
-napoleon_google_docstring = True  # Enable Google-style docstring parsing
