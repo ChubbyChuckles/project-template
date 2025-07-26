e07ec4aa83dc64c7a6a529e3983067f0b954d1d8
ChubbyChuckles <christian.rickert.1989@gmail.com>
2025-07-26 23:26:02 +0200

    fixed setup.py

--- setup.py
+++ setup.py
@@ -8,10 +8,14 @@ setup(
         "numpy",
         "pandas",
         "matplotlib",
+        "sphinx",  # Add for documentation
+        "sphinx-rtd-theme",  # If using Read the Docs theme
+        "sphinxcontrib-napoleon",  # If using Google/NumPy docstrings
+        "pre-commit",  # For pre-commit hooks
     ],
     author="ChubbyChuckles",
     author_email="christian.rickert.1989@gmail.com",
-    description="TODO Add Description",
+    description="A Python project template with automated setup and documentation",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
     url="https://github.com/ChubbyChuckles/project-template",
